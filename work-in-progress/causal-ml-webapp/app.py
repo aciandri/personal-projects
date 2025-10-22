1eca52b95e8de2058dcc0a0193f2bbdedf9f38a2
aciandri <auroraciandri@gmail.com>
2025-10-22 09:16:33 +0200

    logging and base things

--- work-in-progress/causal-ml-webapp/app.py
+++ work-in-progress/causal-ml-webapp/app.py
@@ -1,1 +1,21 @@
+# ----- Libraries -----
 import streamlit as st
+
+import utils
+
+# ----- Logging -----
+logger = utils.get_logger(__name__)
+
+# ----- Page configuration -----
+st.set_page_config(
+    page_title="AI Data Quality",
+    page_icon=":white_check_mark:", # https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
+    layout="wide"
+)
+
+st.markdown(f"<style>{open('settings/style.css').read()}</style>", unsafe_allow_html=True) 
+
+def main():
+    st.write('something')
+
+main()
