1ad17749515b536740c061c7327cf8239e1a676f
aciandri <auroraciandri@gmail.com>
2025-10-22 09:04:43 +0200

    created init and log config

---
+++ work-in-progress/causal-ml-webapp/utils/log_config.py
@@ -0,0 +1,14 @@
+import logging
+from typing import Final
+
+def get_logger(name: str) -> Final:
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        filename="file.log",
+        filemode="a",
+        force=True,
+    )
+    logger: Final = logging.getLogger(name)
+    logger.setLevel(level=logging.WARNING)
