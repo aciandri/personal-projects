51ecd179d825a7574b0edc6912c398138cca9048
aciandri <auroraciandri@gmail.com>
2025-10-22 09:19:14 +0200

    changed the icon to panda face

--- work-in-progress/causal-ml-webapp/app.py
+++ work-in-progress/causal-ml-webapp/app.py
@@ -13,7 +13,7 @@ logger = utils.get_logger(__name__)
 # ----- Page configuration -----
 st.set_page_config(
     page_title="WebApp",
-    page_icon=":white_check_mark:", # https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
+    page_icon=":panda_face:", # https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
     layout="wide"
 )
 
